2ba4171c7eb0e07d9579f2edc621ebc37d961155
karthik24iyer <karthik24iyer@gmail.com>
2024-10-06 16:42:05 +0530

    Pilot Commit

---
+++ src/main/java/com/starwars/StarWarsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsServiceApplication.class, args);
+	}
+
+}
